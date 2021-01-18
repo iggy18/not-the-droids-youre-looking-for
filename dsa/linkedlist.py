6ebcdcc949e981ddf521bb842708ae1df88bb211
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-01-18 09:33:14 -0800

    index search and index replace work

--- dsa/linkedlist.py
+++ dsa/linkedlist.py
@@ -36,6 +36,27 @@ class LinkedList:
                 return True
             node = node.next
         return False
+    
+    def index_search(self, idx):
+        count = 0
+        node = self.head
+        while node:
+            if count == idx:
+                return node.val
+            count += 1
+            node = node.next
+    
+    def index_replace(self, idx, new_val):
+        index = 0
+        node = self.head
+        while not index == idx:
+            index += 1
+            node = node.next
+        node.val = new_val
+
+        
+            
+
 
 ll = LinkedList()
 ll.insert(1)
@@ -48,3 +69,7 @@ ll.prnt()
 print("size" ,ll.size())
 print('Search',ll.search(4))
 print('Search',ll.search(12))
+print('index search 4 =', ll.index_search(4))
+print('index search 1 =', ll.index_search(1))
+ll.index_replace(2, 12)
+ll.prnt()
